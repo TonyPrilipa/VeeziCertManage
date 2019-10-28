0362892195acedbce80e7346b600642338d0cd28
Anton Prilipa <50165409+TonyPrilipa@users.noreply.github.com>
2019-10-28 19:35:25 +0300

    Update session.py

--- session.py
+++ session.py
@@ -3,8 +3,8 @@ import requests
 URL = 'https://auth.veezi.com/token'
 
 def get_token(url):
-    username = 'sysadmin%40cinemaciti.od.ua'
-    password = 'sysadmin7775'
+    username = ''
+    password = ''
     login_query = 'grant_type=password&username={0}&password={1}&client_id=vista_id1'.format(username, password)
 
     request = requests.get(url, data=login_query)

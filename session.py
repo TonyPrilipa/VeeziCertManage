117c06d31d7329c538d4822499718090f3df20b8
TonyPrilipa <tony.prilipa@icloud.com>
2020-01-03 10:55:20 +0200

    First commit

--- session.py
+++ session.py
@@ -3,8 +3,8 @@ import requests
 URL = 'https://auth.veezi.com/token'
 
 def get_token(url):
-    username = ''
-    password = ''
+    username = 'mySuper-username'
+    password = '1234567890joke'
     login_query = 'grant_type=password&username={0}&password={1}&client_id=vista_id1'.format(username, password)
 
     request = requests.get(url, data=login_query)
@@ -16,11 +16,6 @@ def write_html(html):
     file = open('start.html', 'w')
     file.write(html)
     file.close()
-
-
-def some_func(some_arg):
-    cos = 2
-    pass
 
 if __name__ == '__main__':
     html = vsg_login(URL)
